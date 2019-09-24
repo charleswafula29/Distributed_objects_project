768c0b4129ecef188a3d1092e67fe49cdc0af8c3
charleswafula29 <charleswafula1997@gmail.com>
2019-09-24 14:38:18 +0300

    24th Sept updated debugging

--- src/main/java/com/ics/demo/FeignRestClient.java
+++ src/main/java/com/ics/demo/FeignRestClient.java
@@ -1,12 +1,19 @@
 package com.ics.demo;
 
+import com.ics.demo.models.Appointment;
 import com.ics.demo.models.Movie;
+import com.ics.demo.models.Student;
+import feign.Param;
+import feign.QueryMap;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@FeignClient(name = "client", url = "http://10.51.10.111:9090")
+//@FeignClient(name = "client", url = "http://10.51.10.111:9090")
+@FeignClient(name = "client", url = "http://10.55.62.99:1000", configuration = FeignConfig.class)
 public interface FeignRestClient {
 
     @RequestMapping(method = RequestMethod.GET,value = "movies")
@@ -20,4 +27,14 @@ public interface FeignRestClient {
 
     @RequestMapping(method = RequestMethod.PATCH, value = "movies/{id}")
     Movie update(@PathVariable("id") Long id,@RequestBody Movie movie);
+
+    @RequestMapping(method = RequestMethod.POST, value = "students")
+    Student createStudent(@RequestBody Student student);
+
+    @RequestMapping(method = RequestMethod.POST, value = "appointments")
+    Appointment createAppointment(@RequestBody Appointment appointment);
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "appointments/{appointmentId}")
+    Appointment ConfirmAppointment(@PathVariable("appointmentId") Long id,@RequestParam(name = "studentId") Long studentId);
+
 }
